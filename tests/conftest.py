a8bf1abe77c3e2cab369cb3cac1bdfa24897e3e8
Andrei Shabaev <shabaevandrew@gmail.com>
2024-03-28 21:02:33 +0300

    changed base on comments

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,18 +3,19 @@ import zipfile
 import os
 import pytest
 
+CURRENT_FILE = os.path.abspath(__file__)
+CURRENT_DIRECTORY = os.path.dirname(CURRENT_FILE)
+ROOT = os.path.dirname(CURRENT_DIRECTORY)
+RESOURCES_DIR = os.path.join(ROOT, 'resources')
+TEMP_DIR = os.path.join(ROOT, 'temp')
+
 @pytest.fixture(scope='session', autouse=True)
 def create_archive():
-    CURRENT_FILE = os.path.abspath(__file__)
-    CURRENT_DIRECTORY = os.path.dirname(CURRENT_FILE)
-    RESOURSES_DIR = os.path.join(CURRENT_DIRECTORY, 'resources')
-    TEMP_DIR = os.path.join(CURRENT_DIRECTORY, 'temp')
-
-    if not os.path.exists(RESOURSES_DIR):
-        os.mkdir(RESOURSES_DIR)
-    with zipfile.ZipFile(os.path.join(RESOURSES_DIR, 'file.zip'), 'w') as zf:
+    if not os.path.exists(RESOURCES_DIR):
+        os.mkdir(RESOURCES_DIR)
+    with zipfile.ZipFile(os.path.join(RESOURCES_DIR, 'file.zip'), 'w') as zf:
         for file in os.listdir(TEMP_DIR):
             add_file = os.path.join(TEMP_DIR, file)
             zf.write(add_file, os.path.basename(add_file))
     yield
-    shutil.rmtree(RESOURSES_DIR)
+    shutil.rmtree(RESOURCES_DIR)
--- tests/test_read_zip_file.py
+++ tests/test_read_zip_file.py
@@ -1,26 +1,30 @@
+import os
 from zipfile import ZipFile
 from pypdf import PdfReader
 from openpyxl import load_workbook
 import csv
+from tests.conftest import ROOT
 
 
 def test_read_pdf_file():
-    with ZipFile('resources/file.zip') as zip_file:
+    with ZipFile(os.path.join(ROOT, 'resources/file.zip')) as zip_file:
         with zip_file.open('sample-pdf-file.pdf') as pdf_file:
             reader = PdfReader(pdf_file)
             text = reader.pages[0].extract_text()
             assert 'Lorem Ipsum  is simply dummy text of the printing and typesetting industry.' in text
 
+
 def test_read_csv_file():
-    with ZipFile('resources/file.zip') as zip_file:
+    with ZipFile(os.path.join(ROOT, 'resources/file.zip')) as zip_file:
         with zip_file.open('sample-csv-file-for-testing.csv') as csv_file:
             content = csv_file.read().decode('utf-8-sig')
             csvreader = list(csv.reader(content.splitlines()))
             assert csvreader[1][1] == 'Canada'
             assert csvreader[2][1] == 'Germany'
 
+
 def test_read_xlsx_file():
-    with ZipFile('resources/file.zip') as zip_file:
+    with ZipFile(os.path.join(ROOT, 'resources/file.zip')) as zip_file:
         with zip_file.open('sample-xlsx-file-for-testing.xlsx') as xlsx_file:
             workbook = load_workbook(xlsx_file)
             sheet = workbook.active
